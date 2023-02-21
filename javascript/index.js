035493a45154f95cb5b52ffe0f9b525218ae3377
Daniel Bashukoski <58054223+Sparkpy@users.noreply.github.com>
2023-02-21 17:30:12 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,13 @@
-const subtext = document.getElementById("subtext");
-
 var i = 0;
-var txt = "All you need to make homebrew coffee."; /* The text */
-var speed = 25; /* The speed/duration of the effect in milliseconds */
+var txt = 'All you need to make quality homebrew coffee.';
+var speed = 50;
 
 function typeWriter() {
   if (i < txt.length) {
-    subtext.innerHTML += txt.charAt(i);
+    document.getElementById("subtext").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
 
-typeWriter()
+typeWriter();
